bdd67626cfb18a42bc8d043a3f93d30a61e7f7c4
Emmanuel Edward Ntiri <ntiri.e@gmail.com>
2020-08-05 12:02:36 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -40,8 +40,8 @@ setup(
         'keras-contrib', 'pandas', 'numpy', 'plotly', 'PyQt5', 'termcolor'
     ],
     extras_require={
-        "icvmapper": ["tensorflow==2.0.0"],
-        "icvmapper_gpu": ["tensorflow-gpu==2.0.0"],
+        "icvmapper": ["tensorflow"],
+        "icvmapper_gpu": ["tensorflow-gpu"],
     },
     entry_points={'console_scripts': ['icvmapper=icvmapper.cli:main']},
     keywords=[
